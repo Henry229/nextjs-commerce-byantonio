ef68317f4d9318d499d229104a81a91019340177
henry chun <henry.chun@cseducation.com.au>
2023-09-25 09:04:59 +1000

    create modal, dialog

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,15 @@
 'use client';
 
-import Modal from '@/components/ui/modal';
+import { useStoreModal } from '@/hooks/use-store-modal';
+import { useEffect } from 'react';
 
 export default function SetupPage() {
-  return (
-    <div className='p-4'>
-      <Modal title='Test' description='Test Desc' isOpen onClose={() => {}}>
-        Children
-      </Modal>
-    </div>
-  );
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) onOpen();
+  }, [isOpen, onOpen]);
+
+  return <div className='p-4'>Root Page</div>;
 }
