9a831f2fe33ae703c6abe5d231bfa0e08a59f9be
henry chun <henry.chun@cseducation.com.au>
2023-09-25 23:31:47 +1000

    done storeswitcher mainNav

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -18,6 +18,7 @@ import {
 } from '@/components/ui/form';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import toast from 'react-hot-toast';
 
 const formSchema = z.object({
   name: z.string().min(1),
@@ -37,16 +38,22 @@ export function StoreModal() {
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     console.log(values);
-
-    // try {
-    //   setLoading(true);
-    //   const response = await axios.post('/api/stores', values);
-    //   window.location.assign(`/${response.data.id}`);
-    // } catch (error) {
-    //   // toast.error('Something went wrong');
-    // } finally {
-    //   setLoading(false);
-    // }
+    try {
+      setLoading(true);
+      const response = await axios.post('/api/stores', values);
+      // console.log(response.data);
+      // toast.success('Store created..');
+      // axios 에 post하고 돌아오면 stuck이 되는 경우가 많다 그래서
+      // window.location.assign으로 페이지를 refresh하고 response.data.id
+      // redirect하는 효과를 얻는다. window를 쓰는 이유가 refresh이다.
+      // 예를 들어, 만약 response.data.id의 값이 123이라면,
+      // window.location.assign('/123')이 호출되어 사용자는 /123 URL로 이동하게 됩니다.
+      window.location.assign(`/${response.data.id}`);
+    } catch (error) {
+      toast.error('Something went wrong');
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
