c121b69f7a8fd23fd8c7b91595cb746580c9faf5
henry chun <henry.chun@cseducation.com.au>
2023-10-04 21:29:59 +1100

    done category

--- app/(dashboard)/[storeId]/(routes)/categories/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/components/cell-action.tsx
@@ -7,7 +7,7 @@ import {
   DropdownMenuLabel,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-import { BillboardColumn } from './columns';
+import { CategoryColumn } from './columns';
 import { Button } from '@/components/ui/button';
 import { Copy, Edit, MoreHorizontal, Trash } from 'lucide-react';
 import toast from 'react-hot-toast';
@@ -17,7 +17,7 @@ import { AlertModal } from '@/components/modals/alert-modal';
 import axios from 'axios';
 
 interface CellActionProps {
-  data: BillboardColumn;
+  data: CategoryColumn;
 }
 
 export default function CellAction({ data }: CellActionProps) {
@@ -28,18 +28,18 @@ export default function CellAction({ data }: CellActionProps) {
 
   const onCopy = (id: string) => {
     navigator.clipboard.writeText(id);
-    toast.success('Billboard ID copied to clipboard.');
+    toast.success('Category ID copied to clipboard.');
   };
 
   const onDelete = async () => {
     try {
       setLoading(true);
-      await axios.delete(`/api/${params.storeId}/billboards/${data.id}`);
+      await axios.delete(`/api/${params.storeId}/categories/${data.id}`);
       router.refresh();
-      toast.success('Billboard deleted.');
+      toast.success('Category deleted.');
     } catch (error: any) {
       toast.error(
-        'Make sure you removed all categories using this billboard first.'
+        'Make sure you removed all products using this category first.'
       );
     } finally {
       setLoading(false);
@@ -70,7 +70,7 @@ export default function CellAction({ data }: CellActionProps) {
           </DropdownMenuItem>
           <DropdownMenuItem
             onClick={() =>
-              router.push(`/${params.storeId}/billboards/${data.id}`)
+              router.push(`/${params.storeId}/categories/${data.id}`)
             }
           >
             <Edit className='w-4 h-4 mr-2' /> Update
